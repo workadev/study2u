d4c00e5423f4d8dbcdee13096fca1a7d57ac5638
Ayi Budiawan <ayi@virtualspirit.me>
2023-08-16 20:35:32 +0700

    Setup staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
-server '', user: 'ubuntu', roles: %w{app web db}
+server '18.142.32.29', user: 'ubuntu', roles: %w{app web db}
 set :linked_files, %w{ config/database.yml .env.staging config/secrets.yml }
 set :deploy_to, '/home/ubuntu/study2u_backend'
 set :branch, "staging"
